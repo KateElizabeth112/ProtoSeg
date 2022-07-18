de54f2ec5c250bba84aa98254bf2cc9d5896c0fc
Kate Cevora <katecevora@kates-mbp.home>
2022-07-18 11:12:15 +0100

    Updated SLURM run script

--- unet_slurm.sh
+++ unet_slurm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Example of running python script in a batch mode
-#SBATCH -c 1 # Number of CPU Cores
-#SBATCH -p gpus # Partition (queue)
+#SBATCH -c 4 # Number of CPU Cores
+#SBATCH -p gpushigh # Partition (queue)
 #SBATCH --gres gpu:1 # gpu:n, where n = number of GPUs
-#SBATCH --mem 12288 # memory pool for all cores
-#SBATCH --nodelist monal05 # SLURM node
+#SBATCH --mem 20G # memory pool for all cores
+#SBATCH --nodelist monal04 # SLURM node
 #SBATCH --output=slurm.%N.%j.log # Standard output and error log
 
 # Source virtual environment (pip)
